418a7f8e863006eeed17cef825abf8ae3d590a97
sebpaj <sseba07@gmail.com>
2018-06-24 15:32:41 +0200

    changing name convention in few methods

--- backend.py
+++ backend.py
@@ -23,22 +23,22 @@ class Calculations(object):
 
         return g*b*(T1-T2)*pow(x,3)/(ni*alpha)
 
-    def calculatePeclet(self,l,u,alpha):
+    def calculate_peclet(self,l,u,alpha):
 
         return l*u/alpha
 
-    def calculateGrashof(self,g,beta,Ts,Tinf,l,ni):
+    def calculate_grashof(self,g,beta,Ts,Tinf,l,ni):
 
         return g*beta*(Ts-Tinf)*pow(l,3)/(pow(ni,2))
 
-    def calculateFourier(self,alpha,t,l):
+    def calculate_fourier(self,alpha,t,l):
 
         return alpha*t/(pow(l,2))
 
-    def calculateBiot(self,h,l,k):
+    def calculate_biot(self,h,l,k):
 
         return h*l/k
 
-    def calculateStokes(self,t,u,l):
+    def calculate_stokes(self,t,u,l):
 
         return t*u/l
